ed8542b4a8a1d5bb8bceca7bc8466dcd60fb277e
Jiahao Cai <cai.jiah@husky.neu.edu>
2022-02-25 00:21:10 -0500

    fix api url

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,8 @@
 import axios from "axios";
-
-const TUITS_API = "https://cs5500-01-sp22-jiahao-se-node.herokuapp.com/api/tuits";
-const USERS_API = "https://cs5500-01-sp22-jiahao-se-node.herokuapp.com/api/users";
+const BASE_URL = "https://cs5500-01-sp22-jiahao-se-node.herokuapp.com/api";
+// const BASE_URL = "http://localhost:4000/api";
+const TUITS_API = `${BASE_URL}/tuits`;
+const USERS_API = `${BASE_URL}/users`;
 
 export const findAllTuits = () =>
     axios.get(TUITS_API)
