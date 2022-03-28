b7c9461c6e5f7cd8305b02a6d90b44afa217e280
Jiahao Cai <cai.jiah@husky.neu.edu>
2022-03-27 20:48:33 -0400

    added jsdoc for services

--- src/services/dislikes-service.js
+++ src/services/dislikes-service.js
@@ -1,3 +1,6 @@
+/**
+ * @file Implements the service layer that helps fetch dislikes remote API
+ */
 import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
@@ -7,10 +10,22 @@ const api = axios.create({
     withCredentials: true
 });
 
+/**
+ * PUT method for toggling dislikes (dislikes/ undo the dislikes) to a tuit
+ * @param uid User's primary key
+ * @param tid Tuit's primary key
+ * @returns {Promise<AxiosResponse<any>>} Status on whether tuit is disliked or undoing the dislikes
+ * successfully or not
+ */
 export const userTogglesTuitDislikes = (uid, tid) =>
     api.put(`${USERS_API}/${uid}/dislikes/${tid}`)
         .then(response => response.data)
 
+/**
+ * GET method for retrieving all tuits disliked by a particular user
+ * @param uid User's Primary key
+ * @returns {Promise<AxiosResponse<any>>} JSON array contains all tuits disliked by this user
+ */
 export const findAllTuitsDislikedByUser = (uid) =>
     api.get(`${USERS_API}/${uid}/dislikes`)
         .then(response => response.data)
--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,3 +1,6 @@
+/**
+ * @file Implements the service layer that helps fetch tuits remote API
+ */
 import axios from "axios";
 // const BASE_URL = "https://cs5500-node-a3.herokuapp.com/api";
 // const BASE_URL = "http://localhost:4000/api";
@@ -9,30 +12,68 @@ const api = axios.create({
     withCredentials: true
 });
 
+/**
+ * Uses GET to retrieve all tuits from remote sever
+ * @returns {Promise<AxiosResponse<any>>} Body formatted as JSON array containing all the tuits
+ */
 export const findAllTuits = () =>
     api.get(TUITS_API)
         .then(response => response.data);
 
+/**
+ * Uses GET to retrieve a tuit by its primary key
+ * @param tid Tuit's primary key
+ * @returns {Promise<AxiosResponse<any>>} Body formatted as JSON containing the tuit
+ */
 export const findTuitById = (tid) =>
     api.get(`${TUITS_API}/${tid}`)
         .then(response => response.data);
 
+/**
+ * Uses GET to retrieve tuits posted by a particular user
+ * @param uid User's primary key
+ * @returns {Promise<AxiosResponse<any>>} Body formatted as JSON array containing all the tuits
+ * that posted by this user
+ */
 export const findTuitByUser = (uid) =>
     api.get(`${USERS_API}/${uid}/tuits`)
         .then(response => response.data);
 
+/**
+ * Uses POST to create a tuit for a particular user
+ * @param uid User's primary key
+ * @param tuit Tuit's primary key
+ * @returns {Promise<AxiosResponse<any>>} Body formatted as JSON the tuit with
+ * all updated attributes inserted in the database
+ */
 export const createTuitByUser = (uid, tuit) =>
     api.post(`${USERS_API}/${uid}/tuits`, tuit)
         .then(response => response.data);
 
+/**
+ * Uses POST to update a tuit
+ * @param tid Tuit's primary key
+ * @param tuit A tuit object with all updated attributes
+ * @returns {Promise<AxiosResponse<any>>} Body formatted as JSON the tuit inserted in the database
+ */
 export const updateTuit = (tid, tuit) =>
     api.post(`${TUITS_API}/${tid}`, tuit)
         .then(response => response.data);
 
+/**
+ * Uses DELETE to remove a tuit from the database
+ * @param tid Tuit's Primary key
+ * @returns {Promise<AxiosResponse<any>>} Status on whether tuit is removed successfully or not
+ */
 export const deleteTuit = (tid) =>
     api.delete(`${TUITS_API}/${tid}`)
         .then(response => response.data);
 
+/**
+ * Uses DELETE to remove a tuit by its content (For testing purpose)
+ * @param content tuit's content
+ * @returns {Promise<AxiosResponse<any>>} Status on whether tuit is removed successfully or not
+ */
 export const deleteTuitByContent = (content) =>
     api.delete(`${TUITS_API}/byContent/${content}`)
         .then(response => response.data)
