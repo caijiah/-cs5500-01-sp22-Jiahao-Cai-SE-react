9f4da6dfa8e963fa9da10df5b1a44d65a9f17de2
Jiahao Cai <cai.jiah@husky.neu.edu>
2022-03-28 02:06:45 -0400

    added jsdoc for my-dislikes

--- src/components/profile/my-dislikes.js
+++ src/components/profile/my-dislikes.js
@@ -1,9 +1,23 @@
+/**
+ * @file Implements MyDislikes Component for displaying all tuits that user disliked
+ */
 import Tuits from "../tuits";
 import * as service from "../../services/dislikes-service"
 import {useEffect, useState} from "react";
 
+/**
+ * Implements MyDislikes Component for displaying all tuits that user disliked
+ * This page will require user to be logged in
+ * @returns {JSX.Element}
+ */
 const MyDislikes = () => {
+    /**
+     *  to maintain all dislikedTuits state
+     */
     const [dislikedTuits, setDislikedTuits] = useState([]);
+    /**
+     * Calling dislikes service for retrieving all tuit disliked by "me"
+     */
     const findTuitsIDislike = () => {
         service.findAllTuitsDislikedByUser("me")
             .then((tuits) => {
